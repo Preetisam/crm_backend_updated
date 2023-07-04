43b7c2614614d5e2e32fa65623b21778e67f5a2f
Preetisam <preetikamilla@gmail.com>
2023-07-04 15:42:23 +0530

    update

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     DB_NAME: "back_crm",
-    DB_URL: "mongodb://127.0.0.1:27017/back_crm"
+    // DB_URL: "mongodb://127.0.0.1:27017/back_crm"
+    DB_URL: "mongodb+srv://preetikamilla:crm123@projectcrm.s5rj1fl.mongodb.net/"
   };
   
