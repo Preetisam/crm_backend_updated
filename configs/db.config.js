351604d1fb97150946db0b78752b874a26cf9d03
Preetisam <preetikamilla@gmail.com>
2023-07-04 17:54:36 +0530

    upadated

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     DB_NAME: "back_crm",
-    DB_URL: process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/back_crm"
+    DB_URL: "mongodb+srv://preetikamilla:crm123@projectcrm.s5rj1fl.mongodb.net/"
    
   };
   
